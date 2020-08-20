727431ea8a885bf9a27de6c372288e1fe997e172
IolandaDM <68613203+IolandaDM@users.noreply.github.com>
2020-08-20 08:34:19 +0100

    Added current day and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#current-temperature")
     temperatureElement.innerHTML = Math.round(response.data.main.temp)
@@ -10,7 +25,10 @@ function displayTemperature(response) {
     cityElement.innerHTML = response.data.name
     let weatherDescriptionElement = document.querySelector("#weatherDescription")
     weatherDescriptionElement.innerHTML = response.data.weather[0].main
+    let currentDayTimeElement = document.querySelector("#current-day-time")
+    currentDayTimeElement.innerHTML = formatDate(response.data.dt * 1000)
 }
+
 
 
 let apiKey = "667b3c2d5276fd8b0274db2abae287d7";
